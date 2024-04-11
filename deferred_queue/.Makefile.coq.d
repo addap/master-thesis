c89219ea202b69e288cd1c155da05fb30e589ed2
Adrian Dapprich <adrian@dappri.ch>
2024-04-11 11:48:15 +0200

    Prove spec for deferred queue. Also add section 4 (queue), 6 (evaluation) & 7 (conclusion)

---
+++ deferred_queue/.Makefile.coq.d
@@ -0,0 +1,3 @@
+theories/deferred_stack.vo theories/deferred_stack.glob theories/deferred_stack.v.beautified theories/deferred_stack.required_vo: theories/deferred_stack.v 
+theories/deferred_stack.vio: theories/deferred_stack.v 
+theories/deferred_stack.vos theories/deferred_stack.vok theories/deferred_stack.required_vos: theories/deferred_stack.v 
